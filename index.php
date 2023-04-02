0fbe42b374c42ecfa904ec34ca77c510dc9c9a5f
danieldittkowski <daniel.dittkowski@gmail.com>
2023-04-02 12:21:45 +0200

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="styles/index.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <header>
+        <?php include('header.html'); ?>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <?php include('footer.html'); ?>
+    </footer>
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="styles/user.scss">
+        <meta charset="utf-8>">
+    </head>
+    <body>
+        <?php include('header.html')?>
+        <h1>
+            Hier kommen Userinformationen?!
+        </h1>
+    </body>
+</html>
